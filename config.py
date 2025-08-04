2138601076603773d74c03cac837e220a357ef91
kiruba <mkiruba435@gmail.com>
2025-08-04 10:33:01 +0530

    #

--- config.py
+++ config.py
@@ -1,13 +1,4 @@
 
 import streamlit as st
 api_key = st.secrets["OPENROUTER_API_KEY"]
-OPENROUTER_API_KEY = "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"
 OPENROUTER_API_URL = "https://api.openrouter.ai/v1/chat/completions"
-
-
-'''from openai import OpenAI
-
-client = OpenAI(
-  base_url="https://openrouter.ai/api/v1",
-  api_key=api_key,
-)'''
